ab6f324e91cf86fe6547202c9fccd06dd2f46b5f
juuliankim <71047720+juuliankim@users.noreply.github.com>
2021-09-27 18:05:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const session = require('express-session')
+const productos = require('./api/productos')
+const Mensajes = require('./api/mensajes')
 const handlebars = require('express-handlebars')
 const app = express()
 const http = require('http')
@@ -7,19 +8,17 @@ const server = http.Server(app)
 const io = require('socket.io')(server)
 const normalize = require('normalizr').normalize
 const schema = require('normalizr').schema
-const productos = require('./api/productos')
-const Mensajes = require('./api/mensajes')
+const session = require('express-session')
 const passport = require('passport')
-// const bcrypt = require('bcrypt')
-const FacebookStrategy = require('passport-local').Strategy
-const dotenv = require('dotenv')
+const FacebookStrategy = require('passport-facebook').Strategy
 const User = require('./models/users')
+const dotenv = require('dotenv')
+dotenv.config()
+const cluster = require('cluster')
 const { fork } = require('child_process')
 const numCPUs = require('os').cpus().length
 const compression = require('compression')
 const log4js = require('log4js')
-
-dotenv.config()
 
 log4js.configure({
     appenders: {
@@ -34,7 +33,6 @@ log4js.configure({
         warn: { appenders: ["miLoggerWarn"], level: "warn" }
     }
 })
-
 const loggerConsola = log4js.getLogger('consola')
 const loggerWarn = log4js.getLogger('warn')
 const loggerError = log4js.getLogger('error')
@@ -45,15 +43,13 @@ app.use(compression())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
-    secret: 'secret',
+    secret: 'secreto',
     resave: false,
     saveUninitialized: false
 }))
 
-app.use(express.static('public'))
-
-const FACEBOOK_CLIENT_ID = " "
-const FACEBOOK_CLIENT_SECRET = " "
+let FACEBOOK_CLIENT_ID = " "
+let FACEBOOK_CLIENT_SECRET = " "
 
 if (process.argv[4] && process.argv[5]) {
     FACEBOOK_CLIENT_ID = process.argv[4];
@@ -89,46 +85,33 @@ passport.deserializeUser(function (user, done) {
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/info', (req, res) => {
-    let informacion = {}
-    informacion['Argumentos de entrada:'] = `${process.argv[2]} ${process.argv[3]} ${process.argv[4]}`
-    informacion['Nombre de plataforma:'] = process.platform
-    informacion['Version de Node:'] = process.version
-    informacion['Uso de memoria:'] = process.memoryUsage()
-    informacion['Path de ejecucion:'] = process.execPath
-    informacion['Process id:'] = process.pid
-    informacion['Carpeta corriente:'] = process.cwd()
-    informacion['Numero de procesadores'] = numCPUs
-    informacion['Puerto'] = process.argv[2]
-    res.send(JSON.stringify(informacion, null, 4))
-})
+const modoCluster = process.argv[3] == 'cluster'
 
-app.get('/random', (req, res) => {
-    const numeroRandom = fork('./api/numeroRandom.js')
-    let cantidad = 0
-    if (req.query.cant & !isNaN(req.query.cant)) {
-        cantidad = req.query.cant
-    } else if (isNaN(req.query.cant)) {
-        loggerError.error('No se ingresó un número en la ruta /random')
-        res.send('Error:No se ingresó un número')
-    } else {
-        cantidad = 100000000
+if(modoCluster && cluster.isMaster) {
+    for(let i = 0; i < numCPUs; i++) {
+        cluster.fork()
     }
-    numeroRandom.send((cantidad).toString())
-    numeroRandom.on("message", obj => {
-        res.end(JSON.stringify(obj, null, 3))
+    cluster.on('exit', worker => {
+        console.log('Worker', worker.process.pid, 'murio', new Date().toLocaleString())
+        cluster.fork()
     })
-})
+} else {
+    app.get('/info', (req, res) => {
+        let informacion = {}
+        informacion['Argumentos de entrada:'] = `${process.argv[2]} ${process.argv[3]} ${process.argv[4]} ${process.argv[5]}`
+        informacion['Nombre de plataforma:'] = process.platform
+        informacion['Version de Node:'] = process.version
+        informacion['Uso de memoria:'] = process.memoryUsage()
+        informacion['Path de ejecucion:'] = process.execPath
+        informacion['Process id:'] = process.pid
+        informacion['Carpeta corriente:'] = process.cwd()
+        informacion['Numero de procesadores'] = numCPUs
+        informacion['Puerto'] = process.argv[2]
+        res.send(JSON.stringify(informacion, null, 4))
+    })
+}
 
-//
-
-// const MongoStore = require('connect-mongo')
-// const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true}
-
-app.use((err, req, res, next) =>{
-    console.error(err.message)
-    return res.status(500).send('Algo se rompió!!')
-})
+app.use(express.static('public'))
 
 app.engine('hbs', handlebars({
     extname: '.hbs',
@@ -139,18 +122,16 @@ app.engine('hbs', handlebars({
 app.set("view engine", "hbs")
 app.set("views", "./views")
 
-// app.use(session({
-//     store: MongoStore.create({
-//         mongoUrl: 'mongodb+srv://juliankim:coderhouse@cluster0.jiary.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-//         mongoOptions: advancedOptions
-//     }),
-//     secret: 'secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         maxAge: 600000
-//     }
-// }))
+const productosRouter = require('./routes/productosRouter')
+app.use('/api', productosRouter)
+const mensajesRouter = require('./routes/mensajesRouter')
+app.use('/api', mensajesRouter)
+
+//
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
 app.get('/auth/facebook', passport.authenticate('facebook'))
 
@@ -181,26 +162,43 @@ app.get('/logout', (req, res) => {
     res.sendFile(__dirname + '/public/logout.html')
 })
 
-//
-
-app.get('/signup', (req, res) => {
-    res.render('register', {})
+app.get('/info', (req, res) => {
+    let informacion = {}
+    informacion['Argumentos de entrada:'] = `${process.argv[2]} ${process.argv[3]} ${process.argv[4]} ${process.argv[5]}`
+    informacion['Nombre de plataforma:'] = process.platform
+    informacion['Version de Node:'] = process.version
+    informacion['Uso de memoria:'] = process.memoryUsage()
+    informacion['Path de ejecucion:'] = process.execPath
+    informacion['Process id:'] = process.pid
+    informacion['Carpeta corriente:'] = process.cwd()
+    informacion['Numero de procesadores'] = numCPUs
+    informacion['Puerto'] = process.argv[2]
+    res.send(JSON.stringify(informacion, null, 4))
 })
 
-app.post('/signup', passport.authenticate('signup', { failureRedirect: '/failsignup' }), (req, res) => {
-    var user = req.user;
-    res.render('vista', { showLogin: false, showContent: true, bienvenida: user.username, showBienvenida: true });
+app.get('/random', (req, res) => {
+    const numeroRandom = fork('./api/numeroRandom.js')
+    let cantidad = 0
+    if (req.query.cant & !isNaN(req.query.cant)) {
+        cantidad = req.query.cant
+    } else if (isNaN(req.query.cant)) {
+        loggerError.error('No se ingresó un número en la ruta /random')
+        res.send('Error:No se ingresó un número')
+    } else {
+        cantidad = 100000000
+    }
+    numeroRandom.send((cantidad).toString())
+    numeroRandom.on("message", obj => {
+        res.end(JSON.stringify(obj, null, 3))
+    })
 })
 
-app.get('/failsignup', (req, res) => {
-    res.sendFile(__dirname + '/public/failSignup.html')
-})
 
-const productosRouter = require('./routes/productosRouter')
-app.use('/api', productosRouter)
-const mensajesRouter = require('./routes/mensajesRouter')
-const { createHash } = require('crypto')
-app.use('/api', mensajesRouter)
+
+app.use((err, req, res, next) =>{
+    console.error(err.message)
+    return res.status(500).send('Algo se rompió!!')
+})
 
 io.on('connection', async socket => {
     console.log('Usuario conectado')
@@ -243,15 +241,13 @@ io.on('connection', async socket => {
     })
 })
 
-/* let PORT = 0
-if (process.argv[2] && !isNaN(process.argv[2])) {
+let PORT = process.env.PORT
+/* if (process.argv[2] && !isNaN(process.argv[2])) {
     PORT = process.argv[2]
 } else if (isNaN(process.argv[2])) {
     loggerWarn.warn('No se ingresó un puerto válido, se usará el 8080') 
-    PORT = 8080
+    PORT = 8081
 } */
-
-const PORT = process.env.PORT
 
 const svr = server.listen(PORT, () => {
     loggerConsola.info(process.argv)
